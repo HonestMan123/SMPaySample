da1dedcba01a5a6c65cf25520b40c5ebd312448b
HonestMan123 <ljw1245615911@gmail.com>
2017-09-29 10:47:18 +0800

    支付demo

---
+++ app/src/main/java/com/smpay/sample/APPContants.java
@@ -0,0 +1,9 @@
+package com.smpay.sample;
+
+/**
+ * Created by Android(JiaWei) on 2017/9/29.
+ */
+
+public class APPContants {
+    public static final String REQUEST_PARAMS_URL = "http://axgqmq.natappfree.cc/appapi/pay/leguoPaySign";
+}
---
+++ app/src/main/java/com/smpay/sample/MainActivity.java
@@ -0,0 +1,111 @@
+package com.smpay.sample;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.jy.smpay.SMPaySDK;
+import com.jy.smpay.utils.OrderInfoUtils;
+
+import java.net.URLDecoder;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_PAY_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initPay();
+        initViews();
+    }
+
+    private void initViews() {
+        findViewById(R.id.main_pay_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new OrderInfoUtils.OrderTask(MainActivity.this,REQUEST_PAY_CODE).execute("326");
+            }
+        });
+    }
+
+    private void initPay() {
+        SMPaySDK.init(this, APPContants.REQUEST_PARAMS_URL);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data==null||resultCode!=RESULT_OK){
+            return;
+        }
+        switch (requestCode){
+            case REQUEST_PAY_CODE://支付结果回调
+                String payResult = data.getExtras().getString("pay_result");
+                showPayResult(payResult);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showPayResult(String payResult) {
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            String substring = payResult.substring(1);
+            String[] strs = substring.split("&");
+            for (int i = 0; i < strs.length; i++) {
+                switch (i) {
+                    case 0:
+                        stringBuilder.append("支付状态：");
+                        switch (strs[i].substring(strs[i].indexOf("=")+1)) {
+                            case "paid":
+                                stringBuilder.append("支付完成\n");
+                                break;
+                            case "paying":
+                                stringBuilder.append("支付中\n");
+                                break;
+                            case "paid_fail":
+                                stringBuilder.append("支付失败");
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 1:
+                        stringBuilder.append("支付金额：");
+                        stringBuilder.append((Double.parseDouble(strs[i].substring(strs[i].indexOf("=")+1)))/100L);
+                        break;
+                    case 2:
+                        stringBuilder.append("\n订单号：");
+                        stringBuilder.append(strs[i].substring(strs[i].indexOf("=")+1));
+                        break;
+                    case 3:
+                        stringBuilder.append("\n商品名称：");
+                        stringBuilder.append(URLDecoder.decode(strs[i].substring(strs[i].indexOf("=")+1),"UTF-8"));
+                        break;
+                }
+            }
+            payResult = stringBuilder.toString();
+            showPayResultDialog(payResult);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void showPayResultDialog(String result) {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        mBuilder.setTitle("支付结果");
+        mBuilder.setMessage(result);
+        mBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        mBuilder.create().show();
+    }
+}
