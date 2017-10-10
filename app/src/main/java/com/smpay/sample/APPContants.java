9023012d82e76eef5a336d0a2f6d4d97695f2aa5
HonestMan123 <ljw1245615911@gmail.com>
2017-10-10 14:26:01 +0800

    10.10 14:25 更新获取支付订单信息的url

--- app/src/main/java/com/smpay/sample/APPContants.java
+++ app/src/main/java/com/smpay/sample/APPContants.java
@@ -8,5 +8,5 @@ public class APPContants {
     /**
      * 根据订单id获取支付订单信息的请求链接
      */
-    public static final String REQUEST_PARAMS_URL = "http://axgqmq.natappfree.cc/appapi/pay/leguoPaySign";
+    public static final String REQUEST_PARAMS_URL = "http://z7x43t.natappfree.cc/appapi/pay/leguoPaySign";
 }
